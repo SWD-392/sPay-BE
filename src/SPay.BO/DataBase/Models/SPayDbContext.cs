03efca98cbf4fb095a6836b3205040da03fbab7e
Trọng Phan <dev.sytrong@gmail.com>
2024-03-09 13:25:53 +0700

    Update sql server and api

--- src/SPay.BO/DataBase/Models/SPayDbContext.cs
+++ src/SPay.BO/DataBase/Models/SPayDbContext.cs
@@ -35,8 +35,9 @@ namespace SPay.BO.DataBase.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=(local);Database=SPAY-DB-LOCAL;uid=sa;pwd=12345;TrustServerCertificate=True");
-            }
+				//optionsBuilder.UseSqlServer("server=(local);Database=SPAY-DB-LOCAL;uid=sa;pwd=12345;TrustServerCertificate=True");
+				optionsBuilder.UseSqlServer("Server=swd-391.database.windows.net;Database=spay-db;uid=swp;pwd=Spay@123!;TrustServerCertificate=True");
+			}
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
