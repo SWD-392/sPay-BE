c52cbc2af15e974c6eec7e34d7e38a27d98bc08c
Trọng Phan <dev.sytrong@gmail.com>
2024-03-19 11:49:35 +0700

    update basic code

--- src/SPay.API/Controllers/RolesController.cs
+++ src/SPay.API/Controllers/RolesController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SPay.BO.DTOs.Auth.Response;
+using SPay.BO.Extention.Paginate;
+using SPay.Service.Response;
 
 namespace SPay.API.Controllers
 {
@@ -7,5 +10,48 @@ namespace SPay.API.Controllers
 	[ApiController]
 	public class RolesController : ControllerBase
 	{
+		/// <summary>
+		/// Get all GetListRole
+		/// </summary>
+		/// <param name="request"></param>
+		/// <returns></returns>
+		//[Authorize]
+		[ProducesResponseType(typeof(SPayResponse<PaginatedList<GetListRoleResponse>>), StatusCodes.Status200OK)]
+		[HttpGet]
+		public async Task<IActionResult> GetListGetListRole()
+		{
+			//var response = await _service.GetListGetListRolesAsync(request);
+			return Ok(/*response*/);
+		}
+
+		/// <summary>
+		/// Get GetListRole by key
+		/// </summary>
+		/// <param name="key"></param>
+		/// <returns></returns>
+		[HttpGet("{key}")]
+		[ProducesResponseType(typeof(SPayResponse<GetListRoleResponse>), StatusCodes.Status200OK)]
+		public async Task<IActionResult> GetGetListRoleByKeyAsync(string key)
+		{
+			//var response = await _service.GetGetListRoleByKeyAsync(key);
+			return Ok(/*response*/);
+		}
+
+		/// <summary>
+		/// Delete a Role
+		/// </summary>
+		/// <param name="key"></param>
+		[HttpDelete("{key}")]
+		public async Task<IActionResult> DeleteGetListRoleAsync(string key)
+		{
+			//var response = await _service.DeleteGetListRoleAsync(key);
+
+			//if (!response.Success)
+			//{
+			//	return BadRequest(response);
+			//}
+
+			return Ok(/*response*/);
+		}
 	}
 }
